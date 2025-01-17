99134c97d0b286e7fa500584a70290e3f70dc2d0
macbuse <greg.mcshane@gmail.com>
2025-01-17 14:58:10 +0100

    web

---
+++ homotopy.py
@@ -0,0 +1,68 @@
+import plotly.graph_objects as go
+import numpy as np
+
+# Generate curve data
+npts = 100
+n_curves = 100
+
+s = np.linspace(0, 2*np.pi, npts)
+
+def P(z):
+    return z**2 + z + 1
+
+Z = np.exp(1j*s)
+W = P(2*Z)
+Z = Z**2
+
+H = [(1-t)*W + t*Z for t in np.linspace(0,1,n_curves)]
+
+xm = -4
+xM = 8
+ym = -6
+yM = 6
+
+gamma0 = go.Scatter(x=H[0].real, y=H[0].imag, 
+                     mode="lines",
+                     line=dict(width=2, color="red"))
+
+gammat = go.Scatter(x=H[0].real, y=H[0].imag, 
+                     mode="lines",
+                     line=dict(width=2, color="blue"))
+
+gamma1 = go.Scatter(x=H[n_curves-1].real, y=H[n_curves-1].imag, 
+                     mode="lines",
+                     line=dict(width=2, color="red"))
+
+origin = go.Scatter(x=[0], y=[0], mode="markers",
+                    marker=dict(size=10, color="black"))
+
+# Create figure
+fig = go.Figure( data=[gammat, gamma0, gamma1, origin])
+
+fig.update_layout(width=600, height=600,
+
+         xaxis=dict(range=[xm, xM], autorange=False, zeroline=False),
+         yaxis=dict(range=[ym, yM], autorange=False, zeroline=False),
+
+        title_text="Homotopy to a circle ", title_x=0.5,
+
+        updatemenus = [dict(type = "buttons",
+        buttons = [
+            dict(
+
+                args = [None, {"frame": {"duration": 20, 
+                                         "redraw": False
+                                         },
+                                "fromcurrent": True, "transition": {"duration": 10}}],
+
+                label = "Play",
+                method = "animate",
+
+                )])])
+
+fig.update( frames=[go.Frame( data=[go.Scatter(x=H[k].real,
+                                               y=H[k].imag)],
+                             traces=[0]) for k in range(npts) ])
+
+fig.show()
+fig.write_html("homotopy2circle.html")
